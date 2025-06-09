6c687bc3e961e2b193a7450128cf40baf7a0b096
Hector7k <Loza81904gmail.com>
2025-06-08 20:08:24 -0500

    Primer commit: agrego Autonomo.py

---
+++ Autonomo.py
@@ -0,0 +1,29 @@
+import random
+opciones = ['Piedra', 'Papel', 'Tijera']
+computadora = random.choice(opciones)
+print("Piedra")
+print("Papel")
+print("Tijera")
+
+Jugador = input("ELIJE UNA JUGADA: ")
+ 
+if computadora == "Piedra" and Jugador == "Tijera":
+    print("GANA COMPUTADORA")
+elif computadora == "Tijera" and Jugador == "Piedra":
+    print("GANA JUGADOR")
+elif computadora == "Papel" and Jugador == "Piedra":
+    print("GANA COMPUTADORA")
+elif computadora == "Piedra" and Jugador == "Papel":
+    print("GANA JUGADOR")
+elif computadora == "Tijera" and Jugador == "Papel":
+    print("GANA COMPUTADORA")
+elif computadora == "Papel" and Jugador == "Tijera":
+    print("GANA JUGADOR")
+elif computadora == "Piedra" and Jugador == "Piedra":
+    print("EMPATE")
+elif computadora == "Papel" and Jugador == "Papel":
+    print("EMPATE")
+elif computadora == "Tijera" and Jugador == "Tijera":
+    print("EMPATE")
+else:
+    print("Quieres volver a jugar")
