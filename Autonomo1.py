5d1a67011ec104acebff7b5f7909b1cdc1fa5263
Hector7k <loza81904@gmail.com>
2025-06-09 23:05:20 -0500

    Añadir opcines de regreso al menu

--- Autonomo1.py
+++ Autonomo1.py
@@ -1,34 +1,35 @@
 import random
-menu = input("Quieres jugar: ")
-if menu == "Si":
-    print("Vamos a jugar")
-else:
-    print("Chao")
 opciones = ['Piedra', 'Papel', 'Tijera']
 computadora = random.choice(opciones)
-print("Piedra")
-print("Papel")
-print("Tijera")
-
-Jugador = input("ELIJE UNA JUGADA: ")
- 
-if computadora == "Piedra" and Jugador == "Tijera":
-    print("GANA COMPUTADORA")
-elif computadora == "Tijera" and Jugador == "Piedra":
-    print("GANA JUGADOR")
-elif computadora == "Papel" and Jugador == "Piedra":
-    print("GANA COMPUTADORA")
-elif computadora == "Piedra" and Jugador == "Papel":
-    print("GANA JUGADOR")
-elif computadora == "Tijera" and Jugador == "Papel":
-    print("GANA COMPUTADORA")
-elif computadora == "Papel" and Jugador == "Tijera":
-    print("GANA JUGADOR")
-elif computadora == "Piedra" and Jugador == "Piedra":
-    print("EMPATE")
-elif computadora == "Papel" and Jugador == "Papel":
-    print("EMPATE")
-elif computadora == "Tijera" and Jugador == "Tijera":
-    print("EMPATE")
-else:
-    print(menu)
+while True:
+    print("Lista")
+    print("Piedra")
+    print("Papel")
+    print("Tijera")
+    print("salir")
+    Jugador = input("\n seleccione uno: ")
+    
+    if Jugador == "salir":
+        print("Saliendo")
+        break
+    elif Jugador not in ("Piedra", "Papel", "Tijera",):
+        print("opcion no valida vuelve a ingresar")
+        continue
+    elif computadora == "Piedra" and Jugador == "Tijera":
+      print("GANA COMPUTADORA")
+    elif computadora == "Tijera" and Jugador == "Piedra":
+      print("GANA JUGADOR")
+    elif computadora == "Papel" and Jugador == "Piedra":
+      print("GANA COMPUTADORA")
+    elif computadora == "Piedra" and Jugador == "Papel":
+      print("GANA JUGADOR")
+    elif computadora == "Tijera" and Jugador == "Papel":
+      print("GANA COMPUTADORA")
+    elif computadora == "Papel" and Jugador == "Tijera":
+      print("GANA JUGADOR")
+    elif computadora == "Piedra" and Jugador == "Piedra":
+      print("EMPATE")
+    elif computadora == "Papel" and Jugador == "Papel":
+      print("EMPATE")
+    elif computadora == "Tijera" and Jugador == "Tijera":
+     print("EMPATE")
